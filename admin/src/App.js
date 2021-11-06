d179f9a1a59fbc4dfaf41c48e591a32d90635de8
Muhammad Murtaza <48488384+murtaza7799@users.noreply.github.com>
2021-11-06 21:34:34 +0500

    main

--- admin/src/App.js
+++ admin/src/App.js
@@ -5,12 +5,13 @@ import Product from './Component/Product';
 function App() {
   return (
     <div className="App">
-      <Product/>
-      <Product/>
-      <Product/>
-      <Product/>
-      <Product/>
-      <Product/>
+      <Product title="Counter Component" theme="black"/>
+      <Product title="Counter Component" theme="orange"/>
+      <Product title="Counter Component" theme="green"/>
+      <Product title="Counter Component"  theme="indigo"/>
+      <Product title="Counter Component"  theme="blue"/>
+      <Product title="Counter Component"  theme="aqua"/>
+      
     </div>
   );
 }
--- admin/src/Component/Product.jsx
+++ admin/src/Component/Product.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-const Product = () => {
+import Button from '@mui/material/Button';
+const Product = (props) => {
    // let counter = 5;
    const [counter, setCount]=React.useState(5);
+   //console.log(props.theme);
 
     const countUp = () => {
         setCount(counter + 1);
@@ -12,13 +14,25 @@ const Product = () => {
         setCount(counter - 1);
         //counter--;
     }
+  
+    let themeStyle ={
+     color: 'white',
+     backgroundColor: props.theme,
+    
+
+    }
+
    
     return (  
+        
 <div>
-<h1>Counter Component</h1>
- <button onClick={countDown}>-</button> {counter} <button onClick={countUp}>+</button><br/>
+
+<h1 style={themeStyle}>{props.title}</h1>
+ <Button variant="contained" onClick={countDown}>-</Button> {" "+counter +" "} <Button variant="contained" onClick={countUp}>+</Button><br/>
  Current counterValue is: {counter}<br/>
  {counter <0 && <span>Count is less than zero</span>}
+ 
+ <hr/>
 </div>
  
 
